ee48b3003a0a1543043ed353da4fa6b7a4ee11ee
Chris Schneider <chris@scoutapp.com>
2017-04-12 11:20:45 -0600

    Bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ApproximateHistogram.Mixfile do
 
   def project do
     [app: :approximate_histogram,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
